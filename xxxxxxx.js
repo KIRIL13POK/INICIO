6b8993eb417c8e9aeb3b211aab207787a8cecaca
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2022-08-17 19:01:32 +0200

    new
    new

---
+++ xxxxxxx.js
@@ -0,0 +1,26 @@
+/* 1)
+let admin,nombre;
+nombre="John";
+admin=nombre;
+alert(admin);*/
+/*2)
+alert(1/0);// Infinity
+alert("not a number"/5); NaN*/
+/*let a="Pokrovskiy";
+apellido=a;
+alert(apellido);*/
+
+/*let name = "Ilya" ;
+Обратные кавычки вставляют выражение внутри ${...}строки.
+
+alert( `hello ${name}` );*/
+
+/*let age = prompt('How old are you?', 100);
+
+alert(`You are 100 years old!`); // You are 100 years old!*/
+/*let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed*/
+
+/*let nombre = prompt( "¿como es tu nombre?l","kiril");
+alert(nombre);*/
